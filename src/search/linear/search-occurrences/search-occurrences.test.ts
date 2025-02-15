efefbe3c779824205b2860551b90118d98fd0c58
Caio Garcia <caio.garcia@and.digital>
2025-02-15 15:46:12 +0000

    test: extra test

--- src/search/linear/search-occurrences/search-occurrences.test.ts
+++ src/search/linear/search-occurrences/search-occurrences.test.ts
@@ -1,14 +1,14 @@
-import { searchOccurrences } from "./search-occurrences";
+import { searchOccurrences } from './search-occurrences';
 
-describe('searchOccurrences',()=>{
-    it('should successfully return an array of indexes for each occurrence', ()=>{
-        expect(searchOccurrences([2, 3, 5, 6, 12, 32], 2)).toEqual([0])
-        expect(searchOccurrences([2, 2, 5, 6, 12, 32], 2)).toEqual([0,1])
-        expect(searchOccurrences([2, 2, 5, 6, 2, 32], 2)).toEqual([0,1,4])
-        expect(searchOccurrences(['Charlie', 'Charlie', 5, 6, 'Charlie', 'Charlie'], 'Charlie')).toEqual([0,1,4,5])
-    })
-    it('should return an empty array if occurrence is not found', ()=>{
-        expect(searchOccurrences([2, 3, 5, 6, 12, 32], 'Charlie')).toEqual([])
-    })
-
-})
+describe('searchOccurrences', () => {
+  it('should successfully return an array of indexes for each occurrence', () => {
+    expect(searchOccurrences([2, 3, 5, 6, 12, 32], 2)).toEqual([0]);
+    expect(searchOccurrences([2, 2, 5, 6, 12, 32], 2)).toEqual([0, 1]);
+    expect(searchOccurrences([2, 2, 5, 6, 2, 32], 2)).toEqual([0, 1, 4]);
+    expect(searchOccurrences(['Charlie', 'Charlie', 5, 6, 'Charlie', 'Charlie'], 'Charlie')).toEqual([0, 1, 4, 5]);
+  });
+  it('should return an empty array if occurrence is not found', () => {
+    expect(searchOccurrences([2, 3, 5, 6, 12, 32], 'Charlie')).toEqual([]);
+    expect(searchOccurrences([2, 3, 5, 6, 12, 32], undefined)).toEqual([]);
+  });
+});
